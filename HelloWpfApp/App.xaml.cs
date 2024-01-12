f703f0d479f72462eff12cbe4b80c4e7643e71bf
Sadam·SSadik <1903249375@qq.com>
2024-01-12 16:20:52 +0800

    添加项目文件。

---
+++ HelloWpfApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HelloWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
